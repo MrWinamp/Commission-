4a74b779ecd641b690b39a9af3e82a99aad90ee2
Alexander <Alexander-1082@mail.ru>
2021-03-20 10:24:26 +0700

    Шаблонный метод

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
-
+#include <QImage>
+// Задание 1
 class Employee
 {
    int employee_Id;
@@ -16,6 +17,77 @@ public:
 // Этот метод генерирует отчет относительно заданного работника
     void GenerateReport(Employee &em);
 };
+
+// Задание 2
+enum ImageFormat
+{
+   JPG,
+   PNG
+};
+
+class ProcessImage
+{
+public:
+    void process(QString inputFileName, QString outputFileName)
+    {
+        QByteArray imageData = loadImage(inputFileName + ".jpg");
+        QImage image = imageFromData(imageData, ImageFormat::JPG);
+        removeAlphaChannel(image, ImageFormat::PNG);
+        if(needRemoveAlfaChannel())
+            image = processImage(image);
+        imageData = imageData(image, ImageFormat::JPG);
+        saveImage(outputFileName + ".jpg");
+    }
+
+protected:
+    QByteArray loadImage(QString &inputFileName);
+    QImage imageFromData();
+    virtual QImage processImage(QImage &image) = 0;
+    virtual void removeAlphaChannel(QImage &image, ImageFormat format) = 0;
+    QByteArray imageData(QImage &image, ImageFormat format);
+    void saveImage(QString);
+    bool needRemoveAlfaChannel(){
+        return true;
+    }
+
+};
+
+class ProcessJpgImage: public ProcessImage
+{
+protected:
+    bool needRemoveAlfaChannel()
+    {
+        return false;
+    }
+    QImage processImage(QImage &image);
+    void removeAlphaChannel(QImage &image, ImageFormat format);
+};
+
+class ProcessPngImage: public ProcessImage
+{
+protected:
+    QImage processImage(QImage &image);
+    void removeAlphaChannel(QImage &image, ImageFormat format);
+};
+
+void processJpgImage(QString inputFileName, QString outputFileName)
+{
+   QByteArray imageData = loadImage(inputFileName + ".jpg");
+   QImage image = imageFromData(imageData, ImageFormat::JPG);
+   image = processImage(image);
+   imageData = imageData(image, ImageFormat::JPG);
+   saveImage(outputFileName + ".jpg");
+}
+
+void processPngImage(QString inputFileName, QString outputFileName)
+{
+   QByteArray imageData = loadImage(inputFileName + ".png");
+   QImage image = imageFromData(imageData , ImageFormat::PNG);
+   removeAlphaChannel(image, ImageFormat::PNG);
+   image = processImage(image);
+   imageData = imageData(image, ImageFormat::PNG);
+   saveImage(outputFileName + ".png");
+}
 
 
 int main(int argc, char *argv[])
